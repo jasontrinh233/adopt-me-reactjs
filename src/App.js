8b313a9b60232a16906b34c5cbc53f6a5902b9a3
Quang Trinh <jasontrinh233@gmail.com>
2019-06-13 12:47:38 -0700

    Create dropdown for Animal and Breed

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import SearchParams from "./SearchParams";
 
 // Define App element
 const App = () => {
-  return (
-    <div>
-      <h1 id="something-important">Adopt Me!</h1>
-      <SearchParams />
-    </div>
-  );
+   return (
+      <div>
+         <h1 id="something-important">Adopt Me!</h1>
+         <SearchParams />
+      </div>
+   );
 };
 
 // Render App element into DOM
