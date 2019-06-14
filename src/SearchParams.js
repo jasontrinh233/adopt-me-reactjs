9f9343b03dec73e7ade2bbcc6b26cc447ab97ae7
Quang Trinh <jasontrinh233@gmail.com>
2019-06-13 19:45:26 -0700

    Render results from API

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
+import Results from "./Results";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
@@ -10,13 +11,13 @@ const SearchParams = () => {
    const [pets, setPets] = useState([]);
 
    async function requestPets() {
-      const { animal } = await pet.animals({
+      const { animals } = await pet.animals({
          location,
          breed,
          type: animal
       });
 
-      setPets(animal || []);
+      setPets(animals || []);
    }
 
    useEffect(() => {
@@ -50,6 +51,7 @@ const SearchParams = () => {
             <BreedDropdown />
             <button>Submit</button>
          </form>
+         <Results pets={pets} />
       </div>
    );
 };
